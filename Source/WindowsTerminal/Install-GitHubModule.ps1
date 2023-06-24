0738c6490b8a91ec54af1bae1914eb5573c8d558
soroshsabz <soorosh_abi@hotmail.com>
2023-06-25 03:17:10 +0330

    Merged PR 9: Add unit test for Install-GitHubModule. (fixes #4)

--- Source/WindowsTerminal/Install-GitHubModule.ps1
+++ Source/WindowsTerminal/Install-GitHubModule.ps1
@@ -1,19 +1,19 @@
-# ITNOA
+# ITNOA 
 
-New-Variable -Name PROFILE_OPTIONS -Visibility Private -Option Constant -Scope Script -Value @"
+function script:Add-ToProfile() {
+    New-Variable -Name PROFILE_OPTIONS -Visibility Private -Option Constant -Scope Local -Value @"
 # Enable PowerShellForGitHub Module
 
 Import-Module PowerShellForGitHub
-"@ 
+"@
 
-function script:Add-ToProfile() {
     if (-not (Select-String -Path $PROFILE -Pattern "Import-Module PowerShellForGitHub" -SimpleMatch -Quiet)) {
         # TODO: #3 Move configuration to directory instead of manipulating original profile file
         Add-Content -Path $PROFILE -Value $PROFILE_OPTIONS
     }
 }
 
-function Install-GitHubModule() {
+function global:Install-GitHubModule() {
     Install-Module -Name PowerShellForGitHub -Confirm:$false -AcceptLicense
 
     Add-ToProfile
--- Test/WindowsTerminal/Install-GitHubModule.Tests.ps1
+++ Test/WindowsTerminal/Install-GitHubModule.Tests.ps1
@@ -1,13 +1,46 @@
 # ITNOA
 
-# TODO: #4 Add unit test for Install-GitHubModule
-
 BeforeAll {
+    New-Variable -Name PROFILE_OPTIONS_VALUE -Visibility Public -Option Constant -Scope Local -Value "Import-Module PowerShellForGitHub"
 
+    function script:Test-ContentInProfileExists() {
+        return Select-String -Path $PROFILE -Pattern $PROFILE_OPTIONS_VALUE -SimpleMatch -Quiet
+    }
+
+    Mock Uninstall-Module { }
+    Mock Install-Module { }
+
+    # TODO: #8 Why below code does not work
+    if (-not (Test-Path -Path $PROFILE)) {
+        New-Item -Path $PROFILE -ItemType File -Force
+    }
+
+    # Dot sourcing main script
+    . .\Source\WindowsTerminal\Install-GitHubModule.ps1
+
+    . .\Source\WindowsTerminal\Uninstall-GitHubModule.ps1
 }
 
 Describe 'Install-GitHubModule' {
+    BeforeEach {
+        Uninstall-GitHubModule
+    }
+    It 'Calling Install-GitHubModule shoud add correct configuration into profile' {
+        Test-ContentInProfileExists | Should -BeFalse
+        Install-GitHubModule
+        Test-ContentInProfileExists | Should -BeTrue
+    }
     It 'Multiple calling should not be multiple adding module in profile' {
-        Select-String -Path .\Test\WindowsTerminal\TestContent.txt -Pattern "Import-Module" -SimpleMatch -Quiet | Should -BeTrue
+        if (-not (Test-ContentInProfileExists)) {
+            Install-GitHubModule
+        }
+
+        Test-ContentInProfileExists | Should -BeTrue
+        Install-GitHubModule
+        Test-ContentInProfileExists | Should -BeTrue
+        (Get-Content -Path $PROFILE | Select-String -Pattern $PROFILE_OPTIONS_VALUE | Measure-Object -Line).Count | Should -Be 1
+    }
+    AfterEach {
+
     }
 }
